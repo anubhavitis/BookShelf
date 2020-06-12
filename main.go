2f728c3e8808ae19972a8d0aa7e4841f49621d8f
Anubhav Singhal <ss.lfsgd@gmail.com>
2020-06-12 17:58:32 +0530

    form connected

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl = template.Must(template.ParseFiles("index.html"))
+
+//Book structure
+type Book struct {
+	Name    string
+	Author  string
+	Content string
+	Likes   int
+}
+
+//IndexHandler function
+func IndexHandler(w http.ResponseWriter, req *http.Request) {
+	tpl.Execute(w, nil)
+}
 
 func main() {
-	fmt.Printf("Hello World\n")
+	mux := http.NewServeMux()
+	assets := http.FileServer(http.Dir("assets"))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", assets))
+	mux.HandleFunc("/", IndexHandler)
+	mux.HandleFunc("/submit", IndexHandler)
+
+	// port := os.Getenv("PORT")
+	// http.ListenAndServe(":"+port, mux)
+	http.ListenAndServe(":8080", mux)
 }
