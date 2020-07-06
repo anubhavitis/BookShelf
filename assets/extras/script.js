29773415f4c313132e6e839356c7bc24764616f9
Anubhav Singhal <ss.lfsgd@gmail.com>
2020-07-06 12:01:53 +0530

    Repeat password error

--- assets/extras/script.js
+++ assets/extras/script.js
@@ -9,3 +9,17 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+var password = document.getElementById("password")
+  , c_password = document.getElementById("c_password");
+
+function validatePassword(){
+  if(password.value != c_password.value) {
+    c_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    c_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+c_password.onkeyup = validatePassword;
